3d60dc802547512e06de29909ba1272b2697d04b
Anjali Priya <anjalipriya0565@gmail.com>
2023-10-22 17:10:27 +0530

    backend

---
+++ src/config/db.js
@@ -0,0 +1,10 @@
+// require("dotenv").config();
+const mongoose = require("mongoose");
+const connect = async () => {
+  mongoose.set("strictQuery", false);
+  return mongoose.connect(process.env.DB_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+};
+module.exports = connect;
